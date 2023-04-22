0688f2d7fb7a22a1566d00c19f67400c244df048
Bryan Stapleton <90147833+gljusty@users.noreply.github.com>
2023-04-22 08:15:38 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -48,10 +48,6 @@ export default function useCellophane({
     console.log(Object.keys(wrapper));
   }, []);
 
-  /*
-  we typecast here because we know more than typescript does. Functions and objects both extend the object prototype which can be assigned keys in a similar fashion
-  to a dictionary. Those keys can be other functions which behave like methods. We know we've assigned those keys which makes the shape of wrapper consistent with
-  the intended CellophaneWrapper<typeof target> but typescript will throw an error because it only interprets the original function.
-  */
+
   return wrapper as CellophaneWrapper<typeof target>;
 }
